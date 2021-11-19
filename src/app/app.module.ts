31efc5a46701722e7d01499ac9c104c4152d5888
Eli Davidson <edavidson@broadfinancial.com>
2021-11-19 14:08:04 -0500

    add ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveComponentModule } from '@ngrx/component';
 import { AppComponent } from './app.component';
 import { AppDirective } from './app.directive';
 import { MyLetDirective } from './my-let.directive';
@@ -12,6 +13,7 @@ import { MyLetDirective } from './my-let.directive';
   ],
   imports: [
     BrowserModule,
+    ReactiveComponentModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
